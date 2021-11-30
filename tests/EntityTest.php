28b62dc69bf2aefe4cd19b003ba51277e4ff621d
Ryan Bibby <ryan.bibby@ukfast.co.uk>
2021-11-30 10:17:55 +0000

    Remove setUp method from tests for 7.0 support

--- tests/EntityTest.php
+++ tests/EntityTest.php
@@ -7,24 +7,15 @@ use UKFast\SimpleSDK\Entity;
 
 class EntityTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        $this->entity = new Entity([
-            'id' => 1,
-            'first_name' => 'John',
-            'last_name' => 'Doe',
-        ]);
-        
-        parent::setUp();
-
-        return;
-    }
+    protected $entity;
 
     /**
      * @test
      */
     public function sets_attributes_from_an_array()
     {
+        $this->loadSampleEntity();
+
         $this->assertEquals(1, $this->entity->id);
         $this->assertEquals('John', $this->entity->first_name);
         $this->assertEquals('Doe', $this->entity->last_name);
@@ -35,10 +26,21 @@ class EntityTest extends TestCase
      */
     public function can_convert_to_an_array()
     {
+        $this->loadSampleEntity();
+        
         $array = $this->entity->toArray();
 
         $this->assertEquals(1, $array['id']);
         $this->assertEquals('John', $array['first_name']);
         $this->assertEquals('Doe', $array['last_name']);
+    }
+
+    protected function loadSampleEntity()
+    {
+        $this->entity = new Entity([
+            'id' => 1,
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+        ]);
     }
 }
